ccb3082e8f38b3209f629241a07ef4ea40f4680e
euphorialol <nandomagalhaes074@gmail.com>
2022-12-21 18:07:12 -0300

    treinando js

---
+++ aula011/ex008.js
@@ -0,0 +1,5 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} Km/hr`)
+if(vel > 60.5){
+    console.log("Você ultrapassou a velocidade permitida, MULTADO!")
+}
---
+++ aula011/ex009.js
@@ -0,0 +1,4 @@
+var pais =  "EUA"
+if(pais == "Brasil"){
+    console.log("Brasileiro")
+} else {console.log("Estrangeiro")}
