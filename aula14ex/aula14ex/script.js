a875c0d92fa1c0a211c5de4d2050f0618fd1a6bc
euphorialol <nandomagalhaes074@gmail.com>
2022-12-30 15:28:38 -0300

    jquery

---
+++ aula14ex/aula14ex/script.js
@@ -0,0 +1,15 @@
+function contar(){
+    let ini = document.getElementById("txti")
+    let fim = document.getElementById("txtf")
+    let passo = document.getElementById("txtp")
+    let res = document.getElementById("res")
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        .alert("[ERROR] Faltam dados!")
+    } else{
+        res.innerHTML = "Contando: "
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+    }
+}
